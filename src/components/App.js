96c602a1614dc91cc6f49c3e51c9282b21fa24ef
Mbhon1 <Mbhon1>
2023-01-21 13:12:45 +0200

    updated the picture

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ function App() {
           location="Soweto, RSA"
           email="HloniLehlohonolo072@gmail.com"
           phone="(+27) 68-037-9843"
-          // image={require("../images/me.jpeg")}
+          image={require("../images/hloni.jpeg")}
         />
       </div>
       <div className="right column">
@@ -25,15 +25,15 @@ function App() {
           description="I am a very hard working junior marketing manager who is driven, passionate, and individual with a good work ethic. Driven individual who is open minded and self motivated."
         /> 
         {/* <WorkExperience 
-          title="Work Experience"
-          jobTitle1="Jnr Network Admin "
-          company1="Z-Tech Group"
-          duration1="June 2022 - Septemper 2022"
-          description1="Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-          jobTitle2="Help Desk Support"
-          company2="Rosebank College"
-          duration2="January 2017 - February 2018"
-          description2="Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
+          title=""
+          jobTitle1=""
+          company1=""
+          duration1=""
+          description1=""
+          jobTitle2=""
+          company2=""
+          duration2=""
+          description2=""
         /> */}
         <div className="pad-top">
           <Education 
@@ -50,9 +50,9 @@ function App() {
       </div>
     </div>
     {/* <Footer 
-        twitter="https://www.twitter.com/print_whoami/"
-        instagram="https://www.instagram.com/return.whoami/"
-        github="https://github.com/mbhon1/"
+        twitter=""
+        instagram=""
+        github=""
       /> */}
   </main>
   );
